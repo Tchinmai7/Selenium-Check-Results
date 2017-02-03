9305b1ea93f972f3d27926bd2555b5e583251765
Arvind Nedumaran <arvindamirtaa@gmail.com>
2017-02-03 13:05:13 +0530

    Moved comments to readme.

--- check_results.py
+++ check_results.py
@@ -1,15 +1,3 @@
-#Usage - Create an input file with list of reg_no and dob seperated by commas, one per line.
-#Example input.txt follows
-#2012124124124,04-07-1998
-#3241431421421,07-04-1992
-#and so on. To run the script, use python3 check_results.py input.txt
-#The result will be saved as a screenshot with the reg_no as the file name.
-#Needs selenium and python3 installed.
-#Download chrome driver from 
-#https://sites.google.com/a/chromium.org/chromedriver/downloads
-#Copy to /usr/local/bin/ (on *nix based systems)
-##Captcha has to be manually entered. Wait for the script to fill in the regno and date, and then enter captcha.
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
